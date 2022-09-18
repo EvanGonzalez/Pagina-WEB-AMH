b64528337ee7a326d590410f79d430ee3412c2e9
EvanNoNo <50892803+EvanGonzalez@users.noreply.github.com>
2022-09-18 10:40:59 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,47 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.menu', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    
+    sr.reveal('.parrafo1', {
+        duration: 4000,
+        origin: 'left',
+        distance: '-100px'
+    });
+
+    sr.reveal('.colderecha', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.parrafo2', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.video', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+
+    sr.reveal('.parrafo3', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.footer', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-100px'
+    });
+    
+    
