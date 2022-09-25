b7ab66d99742412a230aa35add06e1e779eb8ad6
sora1 <gonzalezevans05@gmail.com>
2022-09-25 16:16:24 -0500

    menu

--- index.php
+++ index.php
@@ -6,12 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/estilo.css" type="text/css">
     <link rel="stylesheet" href="./css/footerstyle.css" type="text/css">
+    <link rel="stylesheet" href="./css/header-style.css" type="text/css">
     <script src="js/scrollreveal.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <title>Pagina Web</title>
 </head>
 
 <body id="fondoMain">
+    <?php
+    include("head.html")
+    ?>
+
+
+
     <div class="row1">
         <?php
         include("Carrusel.html")
