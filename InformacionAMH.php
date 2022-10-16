4260eff586a8ea49be035e736ecca7d04cfca1de
sora1 <gonzalezevans05@gmail.com>
2022-10-16 15:02:36 -0500

    cambios

--- InformacionAMH.php
+++ InformacionAMH.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="./css/footerstyle.css" type="text/css">
     <link rel="stylesheet" href="./css/animacionWeb.css" type="text/css">
     <link rel="stylesheet" href="./css/MigaPan.css" type="text/css">
+    <link rel="stylesheet" href="./css/Modal.css" type="text/css">
     <link rel="stylesheet" href="./css/header-style.css" type="text/css">
     <script src="js/scrollreveal.js"></script>
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
@@ -71,8 +72,9 @@
     </footer>
 
     <!--termina contenendor de informacion-->
-
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+   
     <script src="js/index.js"></script>
     <!--scrip para la animacion de imagenes pagina 2-->
     <script src="js/main.js"></script>
@@ -80,6 +82,7 @@
     <script src="js/directores.js"></script>
     <script src="js/animacion.js"></script>
     <script src="js/boton_up.js"></script>
+    <script src="js/modal.js"></script>
 </body>
 
 </html>
