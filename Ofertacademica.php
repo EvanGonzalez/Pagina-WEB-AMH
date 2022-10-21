9d0886022767bc91fe18ac0962226860967adebb
sora1 <gonzalezevans05@gmail.com>
2022-10-21 14:30:53 -0500

    cambio en oferta academica

--- Ofertacademica.php
+++ Ofertacademica.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="./css/MigaPan.css" type="text/css">
     <link rel="stylesheet" href="./css/header-style.css" type="text/css">
     <link rel="stylesheet" href="./css/infinitocarrusel.css" type="text/css">
+  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="js/scrollreveal.js"></script>
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
@@ -43,7 +44,7 @@
     <div class="container text-center">
 
         <div class="row">
-            <div class="col-12 contenidoderecho">
+            <div class="col-md-12 contenidoderecho">
                 <h3 class="letra3"> Oferta académica </h3>
 
                 <?php
@@ -89,6 +90,7 @@
     <script src="js/Minimenu.js"></script>
     <script src="js/Ofertas.js"></script>
     <script src="js/boton_up.js"></script>
+    
 </body>
 
 </html>
