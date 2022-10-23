e224d99a102dc62219c0c28ccca9c02358fb29f0
Dreoko <omiii0211@gmail.com>
2022-10-23 07:30:00 -0500

    menu

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
     <script src="js/scrollreveal.js"></script>
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
     <title>Escuela Secundaria Ángel María Herrera</title>
 </head>
 
@@ -43,7 +45,7 @@
     <div class="container text-center">
 
         <div class="row">
-            <div class="col-8 contenidoderecho">
+            <div class="col-md-8 contenidoderecho">
                 <h3 class="letra3"> Himno del plantel </h3>
 
                 <?php
@@ -52,7 +54,7 @@
                 include("Inicio/GaleriaImg2.html");
                 ?>
             </div>
-            <div class="col-4 contenidoizquierdo">
+            <div class="col-md-4 contenidoizquierdo">
                 <aside id="asistyle">
                     <h2 class="letra2">Noticias</h2>
 
@@ -82,7 +84,6 @@
     <footer>
         <?php
         include("footer.html");
-
         ?>
     </footer>
 
