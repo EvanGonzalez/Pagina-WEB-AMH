82c4b59586bf2a0b8e8e0b0de38187760750335f
Dreoko <omiii0211@gmail.com>
2022-10-25 13:32:44 -0500

    el fol

--- Otras Actividades/folklor.php
+++ Otras Actividades/folklor.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="../css/header-style.css">
     <link rel="stylesheet" href="../css/footerstyle.css">
     <link rel="stylesheet" href="../Otras Actividades/css/animate.css">
-    <link rel="stylesheet" href="../Otras Actividades/css/banda.css">
+    <link rel="stylesheet" href="../Otras Actividades/css/folklor.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="js/wow.min.js"></script>
@@ -27,14 +27,83 @@
         
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                    <img src="../Otras Actividades/img/folklor/Sin_imagen_disponible.jpg" alt="" class="img-fluid">
-                </div>
-                <div class="col-md-8">
+                <div class="col-md-4 wow fadeInLeft">
+                        <br>
+                        <br>
+                        <br>       
+                        <br>             
+                    <div class="img2">
+                        <img src="../Otras Actividades/img/folklor/Sin_imagen_disponible.jpg" alt="" class="img-fluid">                        
+                    </div>
 
                 </div>
+                <div class="col-md-8 wow fadeInRight">
+                    <h1 class="text-center"> Conjunto Folclórico </h1>
+                    <p>
+                        El Conjunto Típico de la Escuela Secundaria Herrera fue fundado el 26 de julio de 1983 y por iniciativa del conjunto y de los graduandos, 
+                        lo llamaron denominaron “Conjunto de Proyecciones Folclóricas Ángel María Herrera”. Durante un tiempo estuvo integrado hasta por 30 parejas y 70 integrantes.
+                         
+                    </p>
+                    <p>
+                        Esta agrupación fue fundada por los profesores Fermina Carrasco y Ramón Cepeda. Desde su inicio ha tenido destacadas participaciones a nivel 
+                        nacional y ha transmitido un genuino mensaje de la cultura penonomeña. Las primeras prácticas las realizaron en la avenida central, al lado de 
+                        la señorita "Quichi" que pertenecía a un grupo de damas católicas, y posteriormente en las instalaciones del colegio en lugares como: el gimnasio, 
+                        la biblioteca, el salón de audio visual y los pasillos.
+                    </p>
+                </div>
             </div>
+            <div class="row">
+            <div class="card border-dark mb-3 wow fadeInLeft" style="width: 100%;">
+                <div class="card-header">Eventos y Premios</div>
+                <div class="card-body text-dark">
+                    <h5 class="card-title">Eventos Nacionales</h5>
+                    <p class="card-text">
+                        Entre los eventos nacionales en que ha participado podemos mencionar: Festival Toro Guapo de Antón, Festival de la Mejorana de Guararé, La Feria de la Chorrera, Feria de Azuero en Los Santos, Feria San José en David, Feria de la Concepción en Chiriquí y El Desfile de las Polleras en la ciudad capital.
+                    </p>
+                    <p class="card-text">
+                        Entre los premios alcanzados por esta agrupación podemos mencionar que el Ministerio de Educación organizó el concurso de baile para aquellas escuelas que tenían conjuntos y durante los tres años que lo realizaron (Colón, Los Santos y la Cuidad Panamá )  el conjunto de la AMH ganó un primer lugar y dos segundos lugares. 
+                    </p>
+                </div>
+            </div>
+                <div class="col-md-6 wow fadeInLeft">
+                    <div class="card border-dark mb-3 wow fadeInLeft" style="width: 100%;">
+                        <div class="card-header">Bailes y Vestuarios</div>
+                        <div class="card-body text-dark">
+                            <h5 class="card-title">Bailes y Vestuarios</h5>
+                            <p class="card-text">
+                                Algunos bailes que interpreta el conjunto son: El Chogorro, La Iguana, Compadre Gallinazo, la Pachenda y otros. 
+                            </p>
+                            <p class="card-text">
+                               Algunos vestuarios utilizados por los bailarines son: Montunas Coclesanas, Pollera Blanca Tradicional, Polleras Tiriada, Basquiñas, Camisillas Blancas, Coletas marcadas, Cutarras, Chinelas, Sombrero de Junco, sombrero Pintado, Cebaderas, Cabeza de tembleques. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 wow fadeInRight">
+                <div class="card border-dark mb-3" style="width: 100%;">
+                    <div class="card-header">Eventos y Premios</div>
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Eventos Nacionales</h5>
+                        <p class="card-text">
+                            Entre los eventos nacionales en que ha participado podemos mencionar: Festival Toro Guapo de Antón, Festival de la Mejorana de Guararé, La Feria de la Chorrera, Feria de Azuero en Los Santos, Feria San José en David, Feria de la Concepción en Chiriquí y El Desfile de las Polleras en la ciudad capital.
+                        </p>
+                        <p class="card-text">
+                            Entre los premios alcanzados por esta agrupación podemos mencionar que el Ministerio de Educación organizó el concurso de baile para aquellas escuelas que tenían conjuntos y durante los tres años que lo realizaron (Colón, Los Santos y la Cuidad Panamá )  el conjunto de la AMH ganó un primer lugar y dos segundos lugares. 
+                        </p>
+                    </div>
+                </div>                    
+                </div>   
+                               
+            </div>
+
+
         </div>
+        <div class="container-fluid">
+            <div id="registre">
+
+            </div>            
+        </div>
+
     </section>
     <?php include("../Otras Actividades/footer.html");?>
 
