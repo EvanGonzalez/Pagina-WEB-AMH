04b5e674c126d5be70b482a152fe4f7891d26e4a
AngelaYermey <carrionangela53@gmail.com>
2022-10-25 14:14:48 -0500

    hnfvh

--- Formulario1.php
+++ Formulario1.php
@@ -41,155 +41,168 @@
     <!--Inicia contenendor de informacion-->
     <div class="container text-center">
         <div class="contenedorform1">
-            <h1 id="estilodeextoform1">
+            <h1 id="estilodeextoform1" >
                 <div class="cuadrado"></div>
                 <img src="img/2.png" class="imagen2" alt="">
                 MINISTERIO DE EDUCACION<br>
-                ESCUELA SECUNDARIA ANGEL MARIA HERRERA<br>
-                FICHA DE PRE-INSCRIPCION PARA 7 GRADO<br>
-                PRE MEDIA
+                &nbsp;ESCUELA SECUNDARIA<br>
+				ÁNGEL MARÍA HERRERA<br>
+                FICHA DE PRE-INSCRIPCIÓN<br>
+				&nbsp;&nbsp;&nbsp;PARA 7 GRADO PRE MEDIA
             </h1>
         </div>
         <form action="" class="formulario" id="formulario">
-            <!-- Grupo: Usuario -->
-            <div class="formulario__grupo" id="grupo__usuario">
-                <label for="usuario" class="formulario__label">Nombre</label>
-                <div class="formulario__grupo-input">
-                    <input type="text" class="formulario__input" name="nombre" id="nombre" placeholder="Escribe Nombre" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ]+" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">El nombre tiene que ser de 4 a 16 dígitos y solo puede<br> contener numeros, letras y guion bajo.</p>
+			<!-- Grupo: Usuario -->
+			<div class="formulario__grupo" id="grupo__usuario">
+				<label for="usuario" class="formulario__label">Nombre</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input" name="nombre" id="nombre" placeholder="Escribe Nombre">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">El nombre tiene que ser de 4 a 16 dígitos y solo puede<br> contener numeros, letras y guion bajo.</p>
+				
+			</div>
 
-            </div>
+			<!-- Grupo: Nombre -->
+			<div class="formulario__grupo" id="grupo__nombre">
+				<label for="nombre" class="formulario__label">Apellido</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input" name="nombre" id="nombre" placeholder="Escribe Apellido">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">El apellido tiene que ser de 4 a 16 dígitos y solo puede<br> contener numeros, letras y guion bajo.</p>
+			</div>
 
-            <!-- Grupo: Nombre -->
-            <div class="formulario__grupo" id="grupo__nombre">
-                <label for="nombre" class="formulario__label">Apellido</label>
-                <div class="formulario__grupo-input">
-                    <input type="text" class="formulario__input" name="nombre" id="nombre" placeholder="Escribe Apellido" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ]+" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">El apellido tiene que ser de 4 a 16 dígitos y solo puede<br> contener numeros, letras y guion bajo.</p>
-            </div>
+			<!-- Grupo: Contraseña -->
+			<div class="formulario__grupo" id="grupo__password">
+				<label for="password" class="formulario__label">Cédula</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input" name="password" id="password">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">La contraseña tiene que ser de 4 a 12 dígitos.</p>
+			</div>
 
-            <!-- Grupo: Contraseña -->
-            <div class="formulario__grupo" id="grupo__password">
-                <label for="password" class="formulario__label">Cédula</label>
-                <div class="formulario__grupo-input">
-                    <input type="text" class="formulario__input" name="password" id="password" placeholder="0-000-0000" pattern="[E]{0,1}[-]{0,1}([1-9]|1PI|N|PE|10|11|12|13)[-]{1}[0-9]{1,4}[-]{1}[0-9]{1,4}" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">La contraseña tiene que ser de 4 a 12 dígitos.</p>
-            </div>
+			<!-- Grupo: Teléfono -->
+			<div class="formulario__grupo" id="grupo__telefono">
+				<label for="telefono" class="formulario__label">Célular</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input" name="telefono" id="telefono" placeholder="Ejemplo: 6655-6655">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">El telefono solo puede contener numeros y el maximo son 14 dígitos.</p>
+			</div>
 
-            <!-- Grupo: Teléfono -->
-            <div class="formulario__grupo" id="grupo__telefono">
-                <label for="telefono" class="formulario__label">Celular</label>
-                <div class="formulario__grupo-input">
-                    <input type="text" class="formulario__input" name="telefono" id="telefono" placeholder="6000-0000" pattern="[6]{1}[0-9]{3}[-]{1}[0-9]{4}" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">El telefono solo puede contener numeros y el maximo son 14 dígitos.</p>
-            </div>
+			<!-- Grupo: Contraseña 2 -->
+			<div class="formulario__grupo" id="grupo__password2">
+				<label for="password2" class="formulario__label">Dirección</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input nom" name="password2" id="password2">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
+			</div>
+			<br>
 
-            <!-- Grupo: Contraseña 2 -->
-            <div class="formulario__grupo" id="grupo__password2">
-                <label for="password2" class="formulario__label">Dirección</label>
-                <div class="formulario__grupo-input">
-                    <input type="text" class="formulario__input nom" name="password2" id="password2" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
-            </div>
-            <br>
+			<!-- Grupo: Correo Electronico -->
+			<div class="formulario__grupo" id="grupo__correo">
+				<label for="correo" class="formulario__label">Correo Electrónico del Estudiante</label>
+				<div class="formulario__grupo-input">
+					<input type="email" class="formulario__input nom" name="correo" id="correo" placeholder="correo@correo.com">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
+			</div>
 
-            <!-- Grupo: Correo Electronico -->
-            <div class="formulario__grupo" id="grupo__correo">
-                <label for="correo" class="formulario__label">Correo Electrónico del Estudiante</label>
-                <div class="formulario__grupo-input">
-                    <input type="email" class="formulario__input nom" name="correo" id="correo" placeholder="Ejemplo@meduca.edu.pa" title="Utilice el correo institucional @meduca.edu.pa" type="email" pattern=".+@[mM][eE][dD][uU][cC][aA][.][eE][dD][uU][.][pP][aA]" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
-            </div>
+			<br>
 
-            <br>
-            <br>
-            <!-- Grupo: Contraseña 2 -->
-            <div class="formulario__grupo" id="grupo__password2">
-                <label for="password2" class="formulario__label">Escuela de procedencia</label>
-                <div class="formulario__grupo-input">
-                    <input type="text" class="formulario__input nom" name="password2" id="password2" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
-            </div>
-            <br>
+			<!-- Grupo: Contraseña 2 -->
+			<div class="formulario__grupo" id="grupo__password2">
+				<label for="password2" class="formulario__label">Escuela de procedencia</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input nom" name="password2" id="password2">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
+			</div>
+			<br>
 
-            <!-- Grupo: Contraseña 2 -->
-            <div class="formulario__grupo" id="grupo__password2">
-                <label for="password2" class="formulario__label">Nombre del acudiente</label>
-                <div class="formulario__grupo-input">
-                    <input type="text" class="formulario__input" name="password2" id="password2" placeholder="Escribe Nombre" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ]+" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
-            </div>
+			<!-- Grupo: Contraseña 2 -->
+			<div class="formulario__grupo" id="grupo__password2">
+				<label for="password2" class="formulario__label">Nombre del acudiente</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input" name="password2" id="password2">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
+			</div>
 
-            <!-- Grupo: Contraseña 2 -->
-            <div class="formulario__grupo" id="grupo__password2">
-                <label for="password2" class="formulario__label">Apellido del acudiente</label>
-                <div class="formulario__grupo-input">
-                    <input type="text" class="formulario__input" name="password2" id="password2" placeholder="Escribe Apellido" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ]+" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
-            </div>
+			<!-- Grupo: Contraseña 2 -->
+			<div class="formulario__grupo" id="grupo__password2">
+				<label for="password2" class="formulario__label">Apellido del acudiente</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input" name="password2" id="password2">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
+			</div>
 
-            <!-- Grupo: Teléfono -->
-            <div class="formulario__grupo" id="grupo__telefono">
-                <label for="telefono" class="formulario__label"><br>Celular</label>
-                <div class="formulario__grupo-input">
-                    <input type="text" class="formulario__input" name="telefono" id="telefono" placeholder="6000-0000" pattern="[6]{1}[0-9]{3}[-]{1}[0-9]{4}" required>
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">El telefono solo puede contener numeros y el maximo son 14 dígitos.</p>
-            </div>
+			<!-- Grupo: Teléfono -->
+			<div class="formulario__grupo" id="grupo__telefono">
+				<label for="telefono" class="formulario__label nom2">Celular</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input nom3" name="telefono" id="telefono" placeholder="Ejemplo: 6655-6655">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">El telefono solo puede contener numeros y el maximo son 14 dígitos.</p>
+			</div>
 
-            <!-- Grupo: Contraseña 2 -->
-            <div class="formulario__grupo" id="grupo__password2">
-                <label for="password2" class="formulario__label">Fecha de entrega<br> de la ficha de Pre-Inscripción</label>
-                <div class="formulario__grupo-input">
-                    <input type="date" class="formulario__input" name="password2" id="password2" value="<?php echo date("Y-m-d"); ?>" min="<?php echo date("Y-m-d"); ?>" max="<?php echo date("Y-m-d"); ?>">
-                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
-                </div>
-                <p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
-            </div>
+			<br>
 
-            <br>
+			<!-- Grupo: Contraseña 2 -->
+			<div class="formulario__grupo" id="grupo__password2">
+				<label for="password2" class="formulario__label">Fecha de entrega<br> de la ficha de Pre-Inscripción</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input" name="password2" id="password2">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
+			</div>
 
-            <div class="formulario__mensaje" id="formulario__mensaje">
-                <p><i class="fas fa-exclamation-triangle"></i> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
-            </div>
+			<!-- Grupo: Contraseña 2 -->
+			<div class="formulario__grupo" id="grupo__password2">
+				<label for="password2" class="formulario__label"><br>Secretaria</label>
+				<div class="formulario__grupo-input">
+					<input type="text" class="formulario__input" name="password2" id="password2">
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">Ambas contraseñas deben ser iguales.</p>
+			</div>
 
-            <br>
+			<br>
 
-            <div class="formulario__grupo formulario__grupo-btn-enviar">
-                <button type="submit" class="formulario__btn">Enviar</button>
-                <p class="formulario__mensaje-exito" id="formulario__mensaje-exito">Formulario enviado exitosamente!</p>
-            </div>
-        </form>
+			<div class="formulario__mensaje" id="formulario__mensaje">
+				<p><i class="fas fa-exclamation-triangle"></i> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
+			</div>
+
+			<br>
+
+			<div class="formulario__grupo formulario__grupo-btn-enviar">
+				<button type="submit" class="formulario__btn">Enviar</button>
+				<p class="formulario__mensaje-exito" id="formulario__mensaje-exito">Formulario enviado exitosamente!</p>
+			</div>
+		</form>
 
         <br>
         <div class="container2">
             <h1 id="estilodeextoform1">
                 Documentos que debe entregar al momento de inscribirlo(a) COMPLETO:
             </h1>
-            <h2 id="estilodeextoform1">
+            <h2 id="estilodeextoform1" style="text-align: justify;">
                 <li>Certificado de nacimiento escolar (original) o copia de la cédula juvenil por ambos lados. </li>
                 <li>2 fotos tamaño carné actuales
-                    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; **(Varones: corte apropiado y sin cejas sacadas,
-                    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; **&nbsp;Niñas: vestido apropiado (sin escotes, ni maquillajes, ni cabello pintado).
+                    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**&nbsp;(Varones: corte apropiado y sin cejas sacadas).  
+                    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**&nbsp;Niñas: vestido apropiado (sin escotes, ni maquillajes, ni cabello pintado).
                 </li>
                 <li>1 copia del boletín de 6° con los tres trimestres. </li>
                 <li>Copia de la cédula de acudiente.</li>
