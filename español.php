2148c76c901bbce59896c1d10c86a4e0acd31141
sora1 <gonzalezevans05@gmail.com>
2022-10-24 20:23:37 -0500

    cambios en español

--- español.php
+++ español.php
@@ -35,71 +35,83 @@
 
     <!--Inicia contenendor de información-->
     <div class="container text-justify">
-        <div class="row">
-            <div class="col-12 d-flex justify-content-center align-items-center">
+        <div class="animacionespañol">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center align-items-center">
 
-                <div class="card my-4" id="cardEspañol">
-                    <div class="card-body" id="">
-                        <p>
-                            “Lengua castellana mía,<br>
-                            habla de plata y cristal,<br>
-                            ardiente como una llama, <br>
-                            viva cual un manantial”<br>
-                        </p>
-                        <p style="text-align:right;">Juana de Ibarbourou</p>
-
-                    </div>
-                </div>
-
-
-            </div>
-        </div>
-        <div class="row">
-        <p></p>
-        
-        <div class="col-md-6 matematica">
-            <div class="card my-4" style="text-align: justify; background-color:#121b4f; color:white;">
-                <div class="card-body">
-                <p>El Departamento de español de la Escuela Secundaria Ángel María Herrera, consciente de que nuestra lengua debe ser utilizada con propiedad, fluidez y pulcritud, aplica variadas técnicas metodológicas para que el estudiante considere el idioma como un ente vivo. </p>
-                <p>Es responsabilidad del Departamento planificar y ejecutar actividades que permitan descubrir valores y destrezas entre nuestros alumnos y también, enriquecer su nivel cultural.</p>
-                <p>Año tras año, al aproximarse el 23 de abril, Día universal del idioma español, organizamos exposiciones de murales, afiches, alocuciones, separadores de libros, con la finalidad de rendirle homenaje al padre de las letras españolas, Don Miguel de Cervantes Saavedra y, por consiguiente, para ensalzar a la segunda lengua más hablada del mundo.</p>
-                </div>
-            </div>
-
-        </div>
-        <div class="col-md-6"><br>
-            <center><img src="img/Dpt español/IMG_5009.JPG" alt="" width="100%"></center>
-        </div>
-    </div>
-    <div class="row" >
-        <div class="col-md-4">
-            <div class="card my-4" style="background-color:#121b4f; color:white;">
-                <h3 class="card-header" style="background-color:#0079be;text-align:center"><b>Ganadora del Concurso Nacional de Oratoria 2016</b></h3>
-                <div class="card-body">
-                    <div class="thumbnail">
-                        <!-- Incio del thumbnail -->
-                        <center><img src="img/oratoria.jpg" alt="" id="thumb"></center>
-                        <div class="caption" >
-                            <!-- Incio de la clase caption -->
+                    <div class="card my-4" id="cardEspañol">
+                        <div class="card-body" id="">
                             <p>
-                                <center>
-                                    <a href="https://www.tvn-2.com/nacionales/damaris-santamaria-concurso-nacional-oratoria-video_1_1577603.html" class="btn btn-primary" target="blank" style="background-color:white;color:#121b4f">Ver más</a>
-                                </center>
+                                “Lengua castellana mía,<br>
+                                habla de plata y cristal,<br>
+                                ardiente como una llama, <br>
+                                viva cual un manantial”<br>
                             </p>
-                        </div><!-- fin de la clase caption  -->
+                            <p style="text-align:right;">Juana de Ibarbourou</p>
+
+                        </div>
                     </div>
-                    <!--termina thumbnail-->
+
+
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-md-6 matematica">
+                    <div class="animationespañol4">
+                    <div class="card my-4" style="text-align: justify; background-color:#121b4f; color:white;">
+                        <div class="card-body">
+                            <p>El Departamento de español de la Escuela Secundaria Ángel María Herrera, consciente de que nuestra lengua debe ser utilizada con propiedad, fluidez y pulcritud, aplica variadas técnicas metodológicas para que el estudiante considere el idioma como un ente vivo. </p>
+                            <p>Es responsabilidad del Departamento planificar y ejecutar actividades que permitan descubrir valores y destrezas entre nuestros alumnos y también, enriquecer su nivel cultural.</p>
+                            <p>Año tras año, al aproximarse el 23 de abril, Día universal del idioma español, organizamos exposiciones de murales, afiches, alocuciones, separadores de libros, con la finalidad de rendirle homenaje al padre de las letras españolas, Don Miguel de Cervantes Saavedra y, por consiguiente, para ensalzar a la segunda lengua más hablada del mundo.</p>
+                        </div>
+                    </div>
+                    </div>
+                   
+
+                </div>
+
+                <div class="col-md-6"><br>
+                    <center><img src="img/Dpt español/IMG_5009.JPG" alt="" width="100%"></center>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="animationespañol3">
+                    <div class="card my-4" style="background-color:#121b4f; color:white;">
+                        <h3 class="card-header" style="background-color:#0079be;text-align:center"><b>Ganadora del Concurso Nacional de Oratoria 2016</b></h3>
+                        <div class="card-body">
+                            <div class="thumbnail">
+                                <!-- Incio del thumbnail -->
+                                <center><img src="img/oratoria.jpg" alt="" id="thumb"></center>
+                                <div class="caption">
+                                    <!-- Incio de la clase caption -->
+                                    <p>
+                                        <center>
+                                            <a href="https://www.tvn-2.com/nacionales/damaris-santamaria-concurso-nacional-oratoria-video_1_1577603.html" class="btn btn-primary" target="blank" style="background-color:white;color:#121b4f">Ver más</a>
+                                        </center>
+                                    </p>
+                                </div><!-- fin de la clase caption  -->
+                            </div>
+                            <!--termina thumbnail-->
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-8" style="text-align: justify;">
+                <div class="animationespañol2">
+                    <p>Otra fecha de mucho valor en el Departamento es la Semana del libro (del 22 al 29 de septiembre). En este periodo, la meta es promover la lectura tanto en docentes como en educandos, para ello se realizan diversos concursos tales como: lecturas comprensivas, vocabularios, cuentacuentos, corales poéticas, cortometrajes, murales.</p>
+                    <p>Es digno reconocer el apoyo que otrora recibimos de instituciones como la Lotería Nacional de Beneficencia, Universidad Popular de Coclé (actualmente, Universidad del Trabajo), Universidad de Azuero y la Biblioteca Pública Fernando Guardia. De igual forma, valoramos el respaldo incondicional de padres de familia y docentes de los distintos departamentos.</p>
+                    <p>Es importante destacar que, en el año 1988, la escuela Secundaria Ángel María Herrera fue sede del Concurso provincial de oratoria patrocinado por la Caja de Ahorros. En esta ocasión, nuestra representante María Cristina Cheng S, fue la ganadora de la provincia de Coclé y del primer lugar a nivel nacional; hecho histórico ya que era la primera vez, en esta noble región coclesana, que se alcanzaba tal distinción en este renombrado concurso.</p>
+                    <p>En el año 2016, Dámaris Santamaría Reyes, se alzó con el galardón de la duodécima versión del Concurso Nacional de Oratoria cuyo tema fue: “Agua para todos, el reto frente al cambio climático”.</p>
+                    <p>Loor a los estudiantes que han pasado por esta memorable institución y han legado un laurel en la formación cultural de nuestro hermoso pueblo.</p>
                 </div>
             </div>
         </div>
-        <div class="col-md-8" style="text-align: justify;">
-            <p>Otra fecha de mucho valor en el Departamento es la Semana del libro (del 22 al 29 de septiembre). En este periodo, la meta es promover la lectura tanto en docentes como en educandos, para ello se realizan diversos concursos tales como: lecturas comprensivas, vocabularios, cuentacuentos, corales poéticas, cortometrajes, murales.</p>
-            <p>Es digno reconocer el apoyo que otrora recibimos de instituciones como la Lotería Nacional de Beneficencia, Universidad Popular de Coclé (actualmente, Universidad del Trabajo), Universidad de Azuero y la Biblioteca Pública Fernando Guardia. De igual forma, valoramos el respaldo incondicional de padres de familia y docentes de los distintos departamentos.</p>
-            <p>Es importante destacar que, en el año 1988, la escuela Secundaria Ángel María Herrera fue sede del Concurso provincial de oratoria patrocinado por la Caja de Ahorros. En esta ocasión, nuestra representante María Cristina Cheng S, fue la ganadora de la provincia de Coclé y del primer lugar a nivel nacional; hecho histórico ya que era la primera vez, en esta noble región coclesana, que se alcanzaba tal distinción en este renombrado concurso.</p>
-            <p>En el año 2016, Dámaris Santamaría Reyes, se alzó con el galardón de la duodécima versión del Concurso Nacional de Oratoria cuyo tema fue: “Agua para todos, el reto frente al cambio climático”.</p>
-            <p>Loor a los estudiantes que han pasado por esta memorable institución y han legado un laurel en la formación cultural de nuestro hermoso pueblo.</p>
-        </div>
-    </div>
     </div>
     <?php
     include("botonArriba.html");
