0dc274775770a68ebb63c0cf24b225ca0564574d
Dreoko <omiii0211@gmail.com>
2022-10-25 00:02:12 -0500

    a

---
+++ js/pega.js
@@ -0,0 +1,13 @@
+window.onscroll = function(){mifuncion()}
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+console.log(sticky);
+
+function mifuncion(){
+    if (window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
