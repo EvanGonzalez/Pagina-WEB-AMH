8d24ce5f3c09d346f2ecde4cc8c899a080ab9341
sora1 <gonzalezevans05@gmail.com>
2022-10-24 20:58:04 -0500

    carrusel pequeño

--- Ofertacademica.php
+++ Ofertacademica.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="./css/MigaPan.css" type="text/css">
     <link rel="stylesheet" href="./css/header-style.css" type="text/css">
     <link rel="stylesheet" href="./css/infinitocarrusel.css" type="text/css">
-    <link rel="stylesheet" href="efectogaleria.scss" >
+    <link rel="stylesheet" href="/css/carruselPequeño.css" >
     <link rel="shortcut icon" href="img/2.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="js/scrollreveal.js"></script>
@@ -92,6 +92,7 @@
     <script src="js/Ofertas.js"></script>
     <script src="js/boton_up.js"></script>
     <script src="js/efectogaleria.js"></script>
+    <script src="js/CarruselPequeño.js"></script>
 </body>
 
 </html>
