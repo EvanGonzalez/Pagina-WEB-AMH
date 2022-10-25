c7484e65d3bbf2aa44c3ce638d55122d928d5c27
sora1 <gonzalezevans05@gmail.com>
2022-10-24 22:26:26 -0500

    cambios

--- js/CarruselPequeño.js
+++ js/CarruselPequeño.js
@@ -1,42 +1,43 @@
-let carrusel = async () =>{
-    let items = document.querySelectorAll('.itemCarrusel');//Array with all carrusel elements
-    let timeShowingEl = 2000;//Time  to showing the  ItemCarrusel, dont forget sum with timeKeepLastEl
-    let timeKeepLastEl = 3000;//Need to have the same time that CSS properti setted in '.itemCarrusel' -> transition: all 500ms;
-                             //Time  to keep  showing the  ItemCarrusel to dont see the  container Element ".carrusel" 
-                             
-
-    while (true) {
-        for (let i = 0; i < items.length; i++) {
-            if( i==0){
-                items[0].classList.remove( 'position');
-            }
-
-            let element = items[i];
-            element.classList.remove( 'hideItemCarrusel');
-            element.classList.add( 'animationImg','blur');
-            await sleep(timeShowingEl); 
-           
-            if(i == items.length-1){
-                items[0].classList.add( 'position','animationImg','blur');
-                items[0].classList.remove( 'hideItemCarrusel');
-            }
-            else{
-                items[i+1].classList.remove( 'hideItemCarrusel');
-                items[i+1].classList.add( 'animationImg','blur');
-            }
-            await sleep(timeKeepLastEl);
-            element.classList.remove( 'animationImg','blur');
-            element.classList.add( 'hideItemCarrusel');
-        }
-    }
-}
-
-let sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-document.addEventListener('DOMContentLoaded', function(event) {
-  //the event occurred
-  carrusel(); 
-})
-
+var actual = 0;
+			function puntos(n){
+				var ptn = document.getElementsByClassName("punto");
+				for(i = 0; i<ptn.length; i++){
+					if(ptn[i].className.includes("activo")){
+						ptn[i].className = ptn[i].className.replace("activo", "");
+						break;
+					}
+				}
+				ptn[n].className += " activo";
+			}
+			function mostrar(n){
+				var imagenes = document.getElementsByClassName("imagen");
+				for(i = 0; i< imagenes.length; i++){
+					if(imagenes[i].className.includes("actual")){
+						imagenes[i].className = imagenes[i].className.replace("actual", "");
+						break;
+					}
+				}
+				actual = n;
+				imagenes[n].className += " actual";
+				puntos(n);
+			}
+			
+			function siguiente(){
+				actual++;
+				if(actual > 1){
+					actual = 0;
+				}
+				mostrar(actual);
+			}
+			function anterior(){
+				actual--;
+				if(actual < 0){
+					actual = 1;
+				}
+				mostrar(actual);
+			}
+			
+			var velocidad = 3000;
+			var play = setInterval("siguiente()", velocidad);
+			
+		
