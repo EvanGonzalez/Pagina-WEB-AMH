e9d3a6e7622cd07220c50e66e8b4d7d60010d1df
EvanNoNo <50892803+EvanGonzalez@users.noreply.github.com>
2022-11-09 18:06:24 -0500

    inicio de sesion

---
+++ IniciarSesion.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Inicio de sesión</title>
+     <link rel="stylesheet" href="/Iniciosesion/sesion.css">	
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="shortcut icon" href="img/2.ico" type="image/x-icon">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Exo+2&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+    <?php
+    include('./iniciosesion/formulario.html');
+    ?>
+</body>
+</html>
