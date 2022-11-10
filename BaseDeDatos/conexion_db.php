aa5d696279db44d936b4e77a5e9125b07a1f6b38
LuisFranc20 <luisfrancgarcia20@gmail.com>
2022-11-10 16:40:52 -0500

    conexion base de datos, verificacion login, cambio en el action del formulario de login

---
+++ BaseDeDatos/conexion_db.php
@@ -0,0 +1,16 @@
+<?php
+    $host = "localhost:3308";
+    $usuario = "root";
+    $contraseña = "";
+    $db = "db_amh";
+    
+    $conexion = mysqli_connect($host, $usuario, $contraseña, $db);
+
+   
+   /*  if($conexion){
+        echo "Conectado correctamente";
+    }else{
+        echo "Sin conexion";
+    } */
+    
+?>
