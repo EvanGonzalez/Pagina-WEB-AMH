fb53f6a20485c7c2d20b587a7bc32d49a7b73b19
Dreoko <omiii0211@gmail.com>
2022-11-14 06:44:22 -0500

    nnananana

--- Otras Actividades/folklor.php
+++ Otras Actividades/folklor.php
@@ -34,7 +34,7 @@
                         <br>       
                         <br>             
                     <div class="img2">
-                        <img src="../Otras Actividades/img/folklor/Sin_imagen_disponible.jpg" alt="" class="img-fluid">                        
+                        <img src="../Otras Actividades/img/conjunto/conjunto4.jpeg" alt="" class="img-fluid">                        
                     </div>
 
                 </div>
@@ -53,59 +53,117 @@
                     </p>
                 </div>
             </div>
-            <div class="row">
-            <div class="card border-dark mb-3 wow fadeInLeft" style="width: 100%;">
-                <div class="card-header">Eventos y Premios</div>
-                <div class="card-body text-dark">
-                    <h5 class="card-title">Eventos Nacionales</h5>
-                    <p class="card-text">
-                        Entre los eventos nacionales en que ha participado podemos mencionar: Festival Toro Guapo de Antón, Festival de la Mejorana de Guararé, La Feria de la Chorrera, Feria de Azuero en Los Santos, Feria San José en David, Feria de la Concepción en Chiriquí y El Desfile de las Polleras en la ciudad capital.
-                    </p>
-                    <p class="card-text">
-                        Entre los premios alcanzados por esta agrupación podemos mencionar que el Ministerio de Educación organizó el concurso de baile para aquellas escuelas que tenían conjuntos y durante los tres años que lo realizaron (Colón, Los Santos y la Cuidad Panamá )  el conjunto de la AMH ganó un primer lugar y dos segundos lugares. 
-                    </p>
-                </div>
-            </div>
-                <div class="col-md-6 wow fadeInLeft">
-                    <div class="card border-dark mb-3 wow fadeInLeft" style="width: 100%;">
-                        <div class="card-header">Bailes y Vestuarios</div>
-                        <div class="card-body text-dark">
-                            <h5 class="card-title">Bailes y Vestuarios</h5>
-                            <p class="card-text">
-                                Algunos bailes que interpreta el conjunto son: El Chogorro, La Iguana, Compadre Gallinazo, la Pachenda y otros. 
-                            </p>
-                            <p class="card-text">
-                               Algunos vestuarios utilizados por los bailarines son: Montunas Coclesanas, Pollera Blanca Tradicional, Polleras Tiriada, Basquiñas, Camisillas Blancas, Coletas marcadas, Cutarras, Chinelas, Sombrero de Junco, sombrero Pintado, Cebaderas, Cabeza de tembleques. 
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 wow fadeInRight">
-                <div class="card border-dark mb-3" style="width: 100%;">
-                    <div class="card-header">Personajes Destacados</div>
+            <br>
+            <div class="row"></div>
+                <div class="card border-dark mb-3 wow fadeInLeft" style="width: 100%;">
+                    <div class="card-header">Profesor Ramón Cepeda</div>
                     <div class="card-body text-dark">
-                        <h5 class="card-title">Personajes Destacados</h5>
-                        <p class="card-text">
-                        Como bailarines que se han destacado en el conjunto típico podemos mencionar a: Rosalinda Arosemena, Manuel Oliva, Luis Peñalba, Maycol Martínez, Dilcia Sambrano, Jesús Jaén y Jesica Sánchez.  
-                        </p>
                         <h5 class="card-title">Profesor Ramón Cepeda</h5>
                         <p class="card-text">
-                            Fue uno de los fundadores del Conjunto Típico, tiene 65 años de enseñar danza panameña y dirigió el Conjunto Típico de la escuela por 27 años. 
+                        Fue uno de los fundadores del Conjunto Típico, tiene 65 años de enseñar danza panameña y dirigió el Conjunto Típico de la escuela por 27 años. Él llegó en el año 1983 a la escuela y el mismo año tuvo la iniciativa de crear el conjunto, ya que las demás instituciones le preguntaban si la escuela contaba con un Conjunto Típico y como no había, le preguntaban si se podía crear uno. 
                         </p>
+
                     </div>
-                </div>                    
-                </div>   
+                </div>
+                <div class="collapse" id="collapseExample">
+                        <div class="">
+                                <div class="row" id="negar">
+
+                                    <div class="col-md-6 wow fadeInLeft">
+                                        <div class="card border-dark mb-3 wow fadeInLeft" style="width: 100%;">
+                                            <div class="card-header">Eventos y Premios</div>
+                                                <div class="card-body text-dark">
+                                                    <h5 class="card-title">Eventos Nacionales</h5>
+                                                    <p class="card-text">
+                                                    Entre los eventos nacionales en que ha participado podemos mencionar: Festival Toro Guapo de Antón, Festival de la Mejorana de Guararé, La Feria de la Chorrera, Feria de Azuero en Los Santos, Feria San José en David, Feria de la Concepción en Chiriquí y El Desfile de las Polleras en la ciudad capital. 
+                                                    
+                                                    </p>
+                                                    <p class="card-text">
+                                                    En el 2019 el conjunto folclórico participo en diferentes presentaciones tales como: feria de la naranja, feria de Santa Fe, desfile folclórico de Guararé, desfile folclórico de Ocú, desfile folclórico del Torito Guapo en Antón y el desfile folclórico del Sombrero Pintado 
+                                                    </p>
+                                                </div>
+                                            </div>
+                                    </div>
+                                        <div class="col-md-6 wow fadeInRight">
+                                            <div class="card border-dark mb-3" style="width: 100%;">
+                                                <img src="../Otras Actividades/img/conjunto/conjunto3.jpeg" alt="" class="img-fluid">
+                                            </div>                    
+                                        </div>
+                                        <hr>
+                                        <!-- ---- -->
+                                    <div class="col-md-6 wow fadeInRight">
+                                            <div class="card border-dark mb-3" style="width: 100%;">
+                                                <img src="../Otras Actividades/img/conjunto/Premiacion1.jpeg" alt="" class="img-fluid">
+                                            </div>                    
+                                    </div>                                         
+                                    <div class="col-md-6 wow fadeInLeft">
+                                        <div class="card border-dark mb-3 wow fadeInLeft" style="width: 100%;">
+
+                                            <div class="card-header">Premios Alcanzados</div>
+                                                <div class="card-body text-dark">
+                                                    <h5 class="card-title">Premios Alcanzados</h5>
+                                                    <p class="card-text">
+                                                    Entre los premios alcanzados por esta agrupación podemos mencionar que el Ministerio de Educación organizó el concurso de baile para aquellas escuelas que tenían conjuntos y durante los tres años que lo realizaron (Colón, Los Santos y la Cuidad de Panamá) el conjunto de la AMH ganó un primer lugar y dos segundos lugares.
+                                                    </p>
+                                                    <ul>
+                                                        <li>
+                                                            <p>
+                                                            En 2017 el conjunto folclórico obtuvo el primer lugar en tambor 
+                                                            </p>
+                                                        </li>
+                                                        <li>
+                                                            <p>
+                                                            En el 2018 participaron y ganaron concursos obteniendo: 
+                                                            Primer lugar en cumbia, en decima 
+                                                            Segundo lugar en tambor 
+                                                            </p>
+                                                        </li>
+                                                        <li>
+                                                            <p>
+                                                            En el 2019 se participaron de diferentes concursos obteniendo: 
+                                                            Primer lugar en saloma 
+                                                            Segundo lugar en Tambor, y en cumbia    
+
+                                                            </p>
+                                                        </li>
+                                                        <li>
+                                                            <p>
+                                                            En el 2020 por pandemia de la COVID-19, solo se realizaron presentaciones del conjunto folclórico 
+                                                            </p>
+                                                        </li>
+                                                        <li>
+                                                            <p>
+                                                            En 2021 ganaron diferentes concursos entre ellos: 
+                                                            Primer lugar en saloma. 
+                                                            Primer lugar en toque de tambor. 
+                                                            Primer lugar en pareja de baile 
+                                                            </p>
+                                                        </li>
+                                                        <li>
+                                                            <p>
+                                                            Durante este año 2022, el conjunto folclórico fue el primer lugar en tamborera, tambor, saloma. Segundo lugar en cumbia. Tercer lugar en decima 
+                                                            </p>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                        </div>
+                                    </div>
+                                                                   
+                                </div>
+                                    
+                        </div>
+                </div>
+                    <p>
+                    <center>
+                        <a class="btn btn-primary bi bi-card-text" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Leer más
+                        </a>
+                    </center>
+
                                
             </div>
 
         <hr>
-        </div>
-        <div class="container-fluid">
-            <div id="registre">
-                <h2>
-                    Pedir imagenes
-                </h2>
-            </div>            
         </div>
 
     </section>
