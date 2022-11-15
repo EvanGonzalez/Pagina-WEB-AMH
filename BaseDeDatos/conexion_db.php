09b89ac4e8fa8a86823bfdb67e5106d89d8b46bb
Martin-Fuentes <martinfuentes012@gmail.com>
2022-11-14 21:58:22 -0500

    dsdjksjks

--- BaseDeDatos/conexion_db.php
+++ BaseDeDatos/conexion_db.php
@@ -1,16 +1,21 @@
 <?php
-    $host = "localhost:3308";
+    function conectar(){
+    $host = "localhost:3306";
     $usuario = "root";
     $contraseña = "";
     $db = "db_amh";
     
     $conexion = mysqli_connect($host, $usuario, $contraseña, $db);
 
+   return $conexion;
    
-   /*  if($conexion){
+}
+/* if($conexion){
         echo "Conectado correctamente";
     }else{
         echo "Sin conexion";
     } */
+
+
     
 ?>
