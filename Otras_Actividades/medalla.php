3b119ccfb31de539ef7995ac4a5f535bd7f7a7cd
Dreoko <omiii0211@gmail.com>
2022-11-16 23:51:32 -0500

    ya lo ultimo

---
+++ Otras_Actividades/medalla.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../img/2.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../css/footerstyle.css">
+    <link rel="stylesheet" href="../Otras_Actividades/css/animate.css">
+    <link rel="stylesheet" href="../Otras_Actividades/css/gabinete.css">
+    <link rel="stylesheet" href="../css/menutest.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="../Otras_Actividades/js/wow.min.js"></script>
+    <script>
+      new WOW().init();
+    </script> 
+    <title>AMH: Medalla de la Cultura</title>
+</head>
+<body>
+    <?php include ('../Menu_Footer/menumain.html'); ?>
+
+    <section>
+        <div class="container">
+            <br>
+            <h2 class="text-center wow fadeInUp" id="registre">MEDALLA A LA CULTURA – ESCUELA ANGEL MARIA HERRERA</h2>   
+            <br>
+            <div class="row" id="negar">            
+                <div class="col-md-6 wow fadeInLeft">
+                    <p>
+    El Primer Ciclo Secundario de Penonomé fue fundado en 1942.    El historiador Gaspar Rosas Quirós, escribe en su libro “La Educación en Coclé a Partir del XIX” que el 30 de noviembre de 1955, con la Ley de Honores No 79, se honró la memoria de Don Ángel María Herrera dando su nombre al Primer Ciclo Secundario de Penonomé y más tarde, por Ley No 56 del 29 de abril de 1959 el de Escuela Secundaria Ángel María Herrera de Penonomé.
+                    </p>
+                    <p>
+    En 1967 Magdalena Herrera de Pezet instituyó la Medalla de la Cultura Ángel María Herrera en memoria de su padre.   La medalla se entrega anualmente en la ceremonia de graduación de la escuela – desde 1957 hasta 2021. La Medalla es de gran valor en la comunidad Penonomeña, y es un símbolo de orgullo para el Angelino distinguido con ella.
+                    </p>
+                    <p>
+    La escogencia del Medallista está definida por una serie de criterios desarrollados por doña Magdalena, notables penonomeños y autoridades de educación. De estos criterios se destaca que el escogido sería un joven con una mente y personalidad balanceada entre su aplicación al estudio y sus contribuciones a la comunidad que incluye familia, amistades y patria. Sabemos que esa fue la línea de pensamiento de su gestora Magdalena.                    
+                    </p>
+                </div>
+                <div class="col-md-6 wow fadeInRight">
+                    <img src="../Otras_Actividades/img/medalla/Imagen1.jpg" alt="" class="img-fluid rounded-4 shadow p-3 mb-5 bg-body">
+                </div>
+            </div>                 
+        </div>
+
+
+    </section>
+    <section>
+        <div class="container">
+            <h2 class="text-center wow fadeInUp">Medalla Ángel María Herrera</h2>
+            <div class="row" id="negar">
+                <div class="col-md-6 wow fadeInLeft">
+                    <img src="../Otras_Actividades/img/medalla/Imagen2.png" alt="" class="img-fluid rounded-4 shadow p-3 mb-5 bg-body">
+                </div>
+                <div class="col-md-6 wow fadeInRight">
+                    <h2 class="text-center fst-italic">Premio a la cultura en Penonomé</h2>
+                    <p>
+Los medallistas hasta el presente 2021 son: Alicia Brennan, Ana González, Aquiles Martínez, Arístides Rodríguez, Arnulfo Franco, Carmen Madrid, César Galástica, Clara Arosemena, Crescencio Fernández, Cristian Cárdenas, Chao Wen Liu, Desiree Del Rosario, Edgardo Suárez, Edmond Espinosa, Elia Pérez, Elixenia Martínez, Evelia Quiroz, Gladis Lam, Gloria Grimaldo, Hilda Sánchez, Ibeth Rojas, Irania Agrazal, Itzel Lombardo, Ivonne de la Guardia, José Luque, Josefa González, Josefa Martínez, Juan Corro, Julio Lombardo, Karen Yau, Kaven Spark, Lelany Urriola, Lidda Harari, Magdamelcina Ayala, María Eugenia Gómez, María Morales, Marinelda Varela, Marlene Jaén, Minerva Jiménez de Batista, Néstor Martínez, Olmedo Carles, Oxdalia Luna, Rafael Eysseric, Ricaurte Tuñón, Rita Jaén, Rodrigo Guardia, Sergio de León, Teresita Jaén, Yared Cruz y Yarelis Tuñón.
+                    </p>
+                    <p>
+Los medallistas desde 2009 al 2011 son: Ricaurter Henríquez, Judith Jaén, Eyra Herrera y Rosibel Ayala Vásquez.  
+                    </p>
+                    <p>
+Del 2012 al 2018 son: Anazaria Ortega, Juliana Altamiranda, Enrique Campos Escobar, Franklin Barnett, Paola M. Hidalgo Franco, Adriana Valdés, Aurora Osorio Pinzón.
+                    </p>
+                    <p>
+Del 2019 al 2021:  Victoria Lombardo, Angélica González, Rubén Bernal
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include ('../Menu_Footer/footer.html'); ?>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
