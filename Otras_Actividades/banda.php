ae32f7f57b3adaacda39f92583902bd552669325
Dreoko <omiii0211@gmail.com>
2022-11-17 12:48:13 -0500

    nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnoooooooooooooooooooooooooooooooooooooo

--- Otras_Actividades/banda.php
+++ Otras_Actividades/banda.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="../Otras_Actividades/css/animate.css">
     <link rel="stylesheet" href="../Otras_Actividades/css/banda.css">
     <link rel="stylesheet" href="../css/menutest.css">
+    <link rel="stylesheet" href="../Otras_Actividades/css/boton.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
@@ -126,11 +127,12 @@
             </div>
                     
     </section>
+    <?php include '../botonArriba.html'; ?>
 
     <?php
         include '../Menu_Footer/footer.html'; 
     ?>
-    <!-- <script src="../Otras Actividades/js/img_visual.js"></script> -->
+    <script src="../js/boton_up.js"></script>
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
