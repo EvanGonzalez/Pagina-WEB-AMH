146eb1763cbd868880b206f01a50ce71de36c331
Martin-Fuentes <martinfuentes012@gmail.com>
2022-11-19 18:08:06 -0500

    Correciones y ruta corregida

--- Ofertacademica.php
+++ Ofertacademica.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="./css/MigaPan.css" type="text/css">
     <link rel="stylesheet" href="/css/menutest.css" type="text/css">
     <link rel="stylesheet" href="./css/infinitocarrusel.css" type="text/css">
-    <link rel="stylesheet" href="/css/carruselPequeño.css" >
+    <link rel="stylesheet" href="/css/carruselPequeno.css" >
     <link rel="stylesheet" href="/css/galeriadetexto.css" >
     <link rel="shortcut icon" href="img/2.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -82,7 +82,7 @@
     <script src="js/Ofertas.js"></script>
     <script src="js/boton_up.js"></script>
     <script src="js/efectogaleria.js"></script>
-    <script src="js/CarruselPequeño.js"></script>
+    <script src="js/CarruselPequeno.js"></script>
 </body>
 
 </html>
