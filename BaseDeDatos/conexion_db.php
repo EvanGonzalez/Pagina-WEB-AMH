e77d7eb432ccaade7e87e61ed746c2091d58dfdf
LuisFranc20 <luisfrancgarcia20@gmail.com>
2022-11-19 19:57:44 -0500

    cambio name y pass

--- BaseDeDatos/conexion_db.php
+++ BaseDeDatos/conexion_db.php
@@ -1,9 +1,9 @@
 <?php
     function conectar(){
-    $host = "localhost:3306";
-    $usuario = "root";
-    $contraseña = "";
-    $db = "db_amh";
+    $host = "localhost";
+    $usuario = "escuojyx_admin_amh ";
+    $contraseña = "FpghAMG_2k22 ";
+    $db = "escuojyx_basedatos_amh";
     
     $conexion = mysqli_connect($host, $usuario, $contraseña, $db);
 
