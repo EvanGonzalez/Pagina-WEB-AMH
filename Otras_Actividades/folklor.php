9e54ed5d6493edd7e3f867b79b15b3f7d4d46a47
Dreoko <omiii0211@gmail.com>
2022-11-22 03:58:48 -0500

    ya lo ves, la vida es asi

--- Otras_Actividades/folklor.php
+++ Otras_Actividades/folklor.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="../Otras_Actividades/css/animate.css">
     <link rel="stylesheet" href="../Otras_Actividades/css/folklor.css">
     <link rel="stylesheet" href="../Otras_Actividades/css/boton.css">
+    <link rel="stylesheet" href="../css/MigaPan.css" type="text/css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
@@ -21,12 +22,21 @@
 </head>
 <body >
     <?php include ('../Menu_Footer/menumain.html'); ?>
+    <div class="contenedorMigaPan text-center">
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                    include("../Migas_Pan/MigaPan23.html")
+                ?>
+            </div>
+        </div>
+    </div>
     <br>
     <section>
         <div class="container-fluid wow fadeInUp">
             <h1 id="registre" class="text-center">Folclor</h1>
         </div>
-        
+
         <div class="container">
             <div class="row">
                 <div class="col-md-4 wow fadeInLeft">
