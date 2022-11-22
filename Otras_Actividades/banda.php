e784bd11827742cd7bb9ed25baed3a913ddc6726
Dreoko <omiii0211@gmail.com>
2022-11-22 09:40:34 -0500

    f

--- Otras_Actividades/banda.php
+++ Otras_Actividades/banda.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" href="../Otras_Actividades/css/animate.css">
     <link rel="stylesheet" href="../Otras_Actividades/css/banda.css">
     <link rel="stylesheet" href="../css/menutest.css">
-    <link rel="stylesheet" href="../Otras_Actividades/css/boton.css"><link rel="stylesheet" href="../css/MigaPan.css" type="text/css">
+    <link rel="stylesheet" href="../Otras_Actividades/css/boton.css">
+    <link rel="stylesheet" href="../css/MigaPan.css" type="text/css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
