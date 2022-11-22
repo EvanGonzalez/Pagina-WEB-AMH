87fed43a23f861a0621db484d7d880ef2d09ea71
Dreoko <omiii0211@gmail.com>
2022-11-22 04:51:04 -0500

    Sin embargo, las mentiras suelen provocarle ira Por lo cual no puede botar completamente el rencor

--- Otras_Actividades/protec.php
+++ Otras_Actividades/protec.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="../Otras_Actividades/css/protec.css">
     <link rel="stylesheet" href="../css/menutest.css">
     <link rel="stylesheet" href="../Otras_Actividades/css/boton.css">
+    <link rel="stylesheet" href="../css/MigaPan.css" type="text/css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
@@ -22,6 +23,15 @@
 </head>
 <body>
     <?php include ('../Menu_Footer/menumain.html'); ?>
+    <div class="contenedorMigaPan text-center">
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                    include("../Migas_Pan/MigaPan26.html")
+                ?>
+            </div>
+        </div>
+    </div>
     <br>
     <div class="container-fluid">
         <h2 class="text-center" id="registre">PROYECTOS TECNOLÓGICOS</h2>         
